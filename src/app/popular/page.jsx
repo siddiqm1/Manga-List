548b52791c099d49da1af6406488d505f83798e6
Muhammad Sidiq <moehammad.siddiq@gmail.com>
2025-02-24 22:26:32 +0700

    add popular manga page

---
+++ src/app/popular/page.jsx
@@ -0,0 +1,34 @@
+"use client"
+import React, { useEffect, useState } from "react";
+import { getAnimeResponse } from "../libs/api-libs";
+import HeaderMenu from "@/components/utilities/HeaderMenu";
+import MangaList from "@/components/MangaList";
+import Pagination from "@/components/utilities/Pagination";
+
+const Popular = () => {
+  const [page, setPage] = useState(1);
+  const [topManga, setTopManga] = useState([]);
+
+  const fetchData = async () => {
+    const data = await getAnimeResponse("top/manga", `page=${page}`);
+    setTopManga(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [page]);
+
+  return (
+    <div className="">
+      <HeaderMenu title={`Manga Terpopular #${page}`} />
+      <MangaList api={topManga} />
+      <Pagination
+        page={page}
+        setPage={setPage}
+        lastPage={topManga.pagination?.last_visible_page}
+      />
+    </div>
+  );
+};
+
+export default Popular;
---
+++ src/components/utilities/HeaderMenu.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeaderMenu = ({title}) => {
+  return (
+    <div className='text-center text-xl p-2 text-color-accent'>{title}</div>
+  )
+}
+
+export default HeaderMenu
