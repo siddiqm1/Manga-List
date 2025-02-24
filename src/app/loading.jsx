ee83f7be2717d2950c33dffcbcb67def55876ac6
Muhammad Sidiq <moehammad.siddiq@gmail.com>
2025-02-24 23:17:18 +0700

    add not found, loading and search page

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='flex justify-center items-center min-h-screen'>
+        <div className='loader'></div>
+    </div>
+  )
+}
+
+export default Loading
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import InputSearch from "./InputSearch";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <>
       <div className="bg-color-accent flex flex-col justify-between p-4">
-        <h1 className="text-center font-semibold text-2xl">MANGALIST</h1>
+        <Link href={'/'}>
+          <h1 className="text-center font-semibold text-2xl">MANGALIST</h1>
+        </Link>
         <InputSearch />
       </div>
     </>
